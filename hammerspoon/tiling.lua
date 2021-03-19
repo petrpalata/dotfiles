ab1543546f7ef22f97c97e60fb663b910629cefe
Petr Palata <petr.palata@gmail.com>
2021-03-19 09:37:32 +0100

    Add function and hotkey to cycle window among screens to hammerspoon

--- hammerspoon/tiling.lua
+++ hammerspoon/tiling.lua
@@ -1,3 +1,5 @@
+require("hotkeys")
+
 function tileRight()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -24,5 +26,28 @@ function tileLeft()
   win:setFrame(f)
 end
 
+function getWindowScreenIndex(window, screens, screenCount)
+    local windowScreen = window:screen()
+    for i = 1, screenCount do
+        if screens[i] == windowScreen then
+            return i
+        end
+    end
+end
+
+function cycleFocusedWindow()
+    local screens = hs.screen.allScreens()
+    local screenCount = countScreens(screens)
+
+    local focusedWindow = hs.window.focusedWindow()
+    local focusedWindowScreenIndex = getWindowScreenIndex(focusedWindow, screens, screenCount)
+
+    local targetScreenIndex = focusedWindowScreenIndex % screenCount + 1
+    local targetScreen = screens[targetScreenIndex]
+
+    focusedWindow:moveToScreen(targetScreen, true, true, 0)
+end
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", tileRight)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", tileLeft)
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "m", cycleFocusedWindow)
